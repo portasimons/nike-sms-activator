61e2cd24354d5809973552291027624c2b4a859f
Roman <rpistsovfrm@yandex.ru>
2020-04-06 19:39:05 +0500

    – Added finish_activation function – Added SMS requests limit – Deleted UUID library – Deleted API-key value

--- sms_activate.py
+++ sms_activate.py
@@ -1,6 +1,5 @@
 import re
 import requests
-import uuid
 import time
 
 
@@ -25,34 +24,46 @@ def parse_number(response): # extract response phone number (str)
     return response
 
 
-def request_set_status(api_key, operation_id, status): # set activation status
+def request_set_status(api_key, operation_id, status): # set activation status (str)
     request_url = "http://sms-activate.ru/stubs/handler_api.php?api_key=" + api_key + "&action=setStatus&status=" + status + "&id=" + operation_id
     response = requests.post(request_url).text
     return response
 
 
-def request_status(api_key, operation_id): # request current activation status
+def request_status(api_key, operation_id): # request current activation status (str)
     request_url = "http://sms-activate.ru/stubs/handler_api.php?api_key=" + api_key +\
                   "&action=getStatus&id=" + operation_id
     return requests.get(request_url).text
 
 
-def get_sms(api_key, operation_id): # receive SMS from activation service
+def get_sms(api_key, operation_id): # receive SMS from activation service (str)
+    retry_count = 0 # variable to count number of SMS requests.
     while True:
         response = request_status(api_key=api_key, operation_id=operation_id)
         if response.startswith("STATUS_OK:"):
             return response.replace("STATUS_OK:", "")
         if response == "STATUS_WAIT_CODE":
             time.sleep(5)
+            retry_count += 1
             continue
-        #if response == "STATUS_CANCEL"
+        if retry_count == 120: # 120 is a max number of SMS requests
+            return ""
+
+
+def finish_activation(api_key, operation_id, sms_response): # returns true if activation successful (bool)
+    if sms_response == "":
+        response = request_set_status(api_key=api_key, operation_id=operation_id, status="8")
+        return False
+    else:
+        response = request_set_status(api_key=api_key, operation_id=operation_id, status="6")
+        return True
 
 
 def main():
     # parameters
-    api_key = "65300244ffA1d0b9464eAcbAe801bb38"
-    country = "0"
-    service = "ew"
+    api_key = "" # insert API-key here
+    country = "0" # country code
+    service = "ew" # service code
 
     # renting phone number
     get_number_response = get_number(api_key=api_key, service=service, country=country)
@@ -61,6 +72,8 @@ def main():
 
     # starting activation
     sms_response = get_sms(api_key=api_key, operation_id=operation_id)
+    finish_activation(api_key=api_key, operation_id=operation_id, sms_response=sms_response)
+
 
 if __name__ == "__main__":
     main()
